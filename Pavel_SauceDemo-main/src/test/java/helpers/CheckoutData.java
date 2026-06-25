dc345d3e0916eab3caf9a130ef5db0f847fbff00
Paweł <119006902+Pawel-Bukacz@users.noreply.github.com>
2026-06-25 10:05:41 +0200

    Delete Pavel_SauceDemo-main directory

--- Pavel_SauceDemo-main/src/test/java/helpers/CheckoutData.java
+++
@@ -1,8 +0,0 @@
-package helpers;
-
-
-public class CheckoutData {
-    public static final String FIRST_NAME = "John";
-    public static final String LAST_NAME = "Doe";
-    public static final String POSTAL_CODE = "10001";
-}
